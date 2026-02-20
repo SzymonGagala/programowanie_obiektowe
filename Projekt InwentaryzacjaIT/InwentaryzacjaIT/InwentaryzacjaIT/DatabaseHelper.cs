293717480738cb073c0e58452e235df1f37eb7ec
Szymon <Szymon@SZYMON-LAP>
2026-02-20 12:40:15 +0100

    usuwam niepoprawny folder

--- Projekt InwentaryzacjaIT/InwentaryzacjaIT/InwentaryzacjaIT/DatabaseHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InwentaryzacjaIT
-{
-    public static class DatabaseHelper
-    {
-        public static string ConnectionString =
-            @"Server=.\SQLEXPRESS;Database=InwentaryzacjaIT;Trusted_Connection=True;TrustServerCertificate=True;";
-    }
-}
--- Projekt InwentaryzacjaIT/InwentaryzacjaIT/InwentaryzacjaIT/Models/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InwentaryzacjaIT.Models
-{
-    public abstract class BaseEntity
-    {
-        // klucz główny w bazie danych
-        public int Id { get; set; }
-    }
-}
